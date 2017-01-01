41cf7d26a4991c48e11ab98713dd8a2dcebc31fd
TinoM <TinoM@users.noreply.github.com>
2017-01-01 18:41:23 +0100

    add sdl_net,sdl_image

--- sdl+libogg+libvorbis+sdl_mixer+vorbis-tools.sh
+++ sdl+libogg+libvorbis+sdl_mixer+vorbis-tools.sh
@@ -7,6 +7,10 @@ untar_file libogg-1.3.2.tar
 untar_file libvorbis-1.3.5.tar
 untar_file SDL2_mixer-2.0.1.tar --exclude=SDL2_mixer-2.0.1/Xcode
 untar_file vorbis-tools-1.4.0.tar
+extract_file SDL2_net-2.0.1.tar
+extract_file SDL2_image-2.0.1.zip
+extract_file freetype-2.7.1.tar
+extract_file SDL2_ttf-2.0.14.zip
 
 patch -d /c/temp/gcc/SDL2-2.0.6 -p1 < sdl-clipcursor.patch
 
@@ -60,6 +64,54 @@ make install
 cd /c/temp/gcc
 rm -rf build src
 
+mv SDL2_net-2.0.1 src
+mkdir build
+cd build
+
+../src/configure --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 \
+--prefix=/c/temp/gcc/dest --disable-shared "CFLAGS=-s -O3" || fail_with SDL_net 1 - EPIC FAIL
+
+make $X_MAKE_JOBS all || fail_with SDL_net 2 - EPIC FAIL
+make install || fail_with SDL_net 3 - EPIC FAIL
+cd /c/temp/gcc
+rm -rf build src
+
+mv SDL2_image-2.0.1 src
+mkdir build
+cd build
+
+../src/configure --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 \
+--prefix=/c/temp/gcc/dest --disable-shared "CFLAGS=-s -O3" || fail_with SDL_image 1 - EPIC FAIL
+
+make $X_MAKE_JOBS all || fail_with SDL_image 2 - EPIC FAIL
+make install || fail_with SDL_image 3 - EPIC FAIL
+cd /c/temp/gcc
+rm -rf build src
+
+mv freetype-2.7.1 src
+mkdir build dest
+cd build
+
+../src/configure --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 --disable-shared \
+--prefix=/c/temp/gcc/dest "CFLAGS=-s -O3" || fail_with freetype 1 - EPIC FAIL
+
+make $X_MAKE_JOBS all || fail_with freetype 2 - EPIC FAIL
+make install || fail_with freetype 3 - EPIC FAIL
+cd /c/temp/gcc
+rm -rf build src
+
+mv SDL2_ttf-2.0.14 src
+mkdir build
+cd build
+
+../src/configure --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 \
+--prefix=/c/temp/gcc/dest --disable-shared "CFLAGS=-s -O3" || fail_with SDL_ttf 1 - EPIC FAIL
+
+make $X_MAKE_JOBS all || fail_with SDL_ttf 2 - EPIC FAIL
+make install || fail_with SDL_ttf 3 - EPIC FAIL
+cd /c/temp/gcc
+rm -rf build src
+
 mv vorbis-tools-1.4.0 src
 mkdir build
 cd build
@@ -74,7 +126,7 @@ rm -rf build src
 
 mv dest SDL+libogg+libvorbis+SDL_mixer+vorbis-tools
 cd SDL+libogg+libvorbis+SDL_mixer+vorbis-tools
-rm -rf bin/sdl2-config lib/cmake lib/pkgconfig lib/*.la share
+rm -rf bin/sdl2-config bin/freetype-config lib/cmake lib/pkgconfig lib/*.la share
 for i in bin/*.exe; do mv $i ${i/x86_64-w64-mingw32-}; done
 
 7z -mx0 a ../SDL+libogg+libvorbis+SDL_mixer+vorbis-tools.7z *
