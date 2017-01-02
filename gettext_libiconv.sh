7099dc2b55c504c537611aa4c013a5a7abe596c4
TinoM <TinoM@users.noreply.github.com>
2017-01-02 17:14:07 +0100

    ICU

--- gettext_libiconv.sh
+++ gettext_libiconv.sh
@@ -4,8 +4,21 @@ source ./0_append_distro_path.sh
 
 extract_file libiconv-1.14.tar
 extract_file gettext-0.19.8.tar
+# extract_file libxml2-2.9.4.tar
 
 cd /c/temp/gcc
+
+# mv libxml2-2.9.4 src
+# mkdir build
+# cd build
+
+# ../src/configure --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 \
+# --prefix=/c/temp/gcc/dest --disable-shared "CFLAGS=-s -O3" || fail_with LIBXML2 1 - EPIC FAIL
+
+# make $X_MAKE_JOBS all || fail_with LIBXML2 2 - EPIC FAIL
+# make install || fail_with LIBXML2 3 - EPIC FAIL
+# cd /c/temp/gcc
+# rm -rf build src
 
 mv libiconv-1.14 src
 mkdir build
@@ -26,7 +39,7 @@ mkdir build
 cd build
 
 ../src/configure --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 \
---prefix=/c/temp/gcc/dest --disable-shared "CFLAGS=-s -O3" || fail_with GETTEXT 1 - EPIC FAIL
+--prefix=/c/temp/gcc/dest --disable-shared --enable-static "CFLAGS=-s -O3" "CPPFLAGS=-DLIBXML_STATIC" || fail_with GETTEXT 1 - EPIC FAIL
 
 make $X_MAKE_JOBS all || fail_with GETTEXT 2 - EPIC FAIL
 make install || fail_with GETTEXT 3 - EPIC FAIL
@@ -45,3 +58,8 @@ make install || fail_with LIBICONV 3 - EPIC FAIL
 cd /c/temp/gcc
 rm -rf build src
 
+mv dest gettext+libiconv
+cd gettext+libiconv
+rm -rf lib/cmake lib/pkgconfig lib/*.la share
+
+7z -mx0 a ../gettext+libiconv.7z *
---
+++ libicu.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+source ./0_append_distro_path.sh
+
+extract_file icu4c-57_1-src.tar
+
+cd /c/temp/gcc
+mv icu src
+mkdir build dest
+cd build
+
+CFLAGS="-s -O3" ../src/source/RunConfigureICU MinGW --prefix=/c/temp/gcc/dest --disable-renaming --disable-rpath --enable-static --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 --disable-shared || fail_with freetype 1 - EPIC FAIL
+
+make $X_MAKE_JOBS all || fail_with freetype 2 - EPIC FAIL
+make install || fail_with freetype 3 - EPIC FAIL
+cd /c/temp/gcc
+rm -rf build src
+mv dest icu4c-57_1
+cd icu4c-57_1
+rm -rf bin lib/pkgconfig lib/*.la share
+
+7z -mx0 a ../icu4c-57_1.7z *
