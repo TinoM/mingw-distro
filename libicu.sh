593aa517d3a21b64c3997af55daa96fafc468834
TinoM <TinoM@users.noreply.github.com>
2017-11-30 13:25:18 +0100

    update ICU

--- libicu.sh
+++ libicu.sh
@@ -1,22 +1,20 @@
 #!/bin/sh
-
 source ./0_append_distro_path.sh
 
-extract_file icu4c-57_1-src.tar
+#untar_file icu4c-60_1-src.tar
 
-cd /c/data/Temp/gcc
-mv icu src
-mkdir build dest
+cd /c/Temp/gcc
+#mv icu src
+#mkdir build dest
 cd build
 
-CFLAGS="-s -O3" ../src/source/RunConfigureICU MinGW --prefix=/c/data/Temp/gcc/dest --disable-renaming --disable-rpath --enable-static --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 --disable-shared || fail_with freetype 1 - EPIC FAIL
-
-make $X_MAKE_JOBS all || fail_with freetype 2 - EPIC FAIL
-make install || fail_with freetype 3 - EPIC FAIL
-cd /c/data/Temp/gcc
+LIBS="-lmsvcr100" CFLAGS="-s -DMINGW_HAS_SECURE_API" CXXFLAGS="-s -DMINGW_HAS_SECURE_API" ../src/source/RunConfigureICU MinGW --prefix=/c/Temp/gcc/dest --disable-renaming --disable-rpath --enable-release --disable-debug --disable-shared --enable-static --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32
+make $X_MAKE_JOBS all
+make install
+cd /c/Temp/gcc
 rm -rf build src
-mv dest icu4c-57_1
-cd icu4c-57_1
+mv dest icu4c-60_1
+cd icu4c-60_1
 rm -rf bin lib/pkgconfig lib/*.la share
 
-7z -mx0 a ../icu4c-57_1.7z *
+7z -mx0 a ../icu4c-60_1.7z *
